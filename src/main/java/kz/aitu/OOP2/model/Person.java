f6dd0408436203e09aeb9e9dca646fe510cabc1b
Zero201 <rahat201.m@gmail.com>
2026-02-05 23:47:13 +0500

    change project

--- src/main/java/kz/aitu/OOP2/model/Person.java
+++ src/main/java/kz/aitu/OOP2/model/Person.java
@@ -5,8 +5,6 @@ public abstract class Person {
     protected int id;
     protected String name;
 
-    public Person() {}
-
     public Person(int id, String name) {
         this.id = id;
         this.name = name;
